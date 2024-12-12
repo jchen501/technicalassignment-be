a16b9b2711b0c4dab828de3eabf42e0258e4b183
jchen501 <jiayinggg98@gmail.com>
2024-12-12 11:29:04 +0800

    Store CSV file

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 const express = require('express');
-const app = express();
+const cors = require('cors');
+const multer = require('multer');
 
-app.use(express.json());
-app.listen(3000, () => console.log(`Listening on port 3000`));
+const app = express();
+app.use(express.json())
+app.use(cors())
+
+const storage = multer.diskStorage({
+    destination: function(req,file,cb){
+        return cb(null, './public')
+    },
+    filename: function(req,file,cb){
+        return cb(null, file.originalname)
+    }
+})
+    
+const upload = multer({storage})
+
+app.post('/upload', upload.single('file'), (req, res) => {
+    res.sendStatus(200)
+});
+
+app.listen(8080, () => console.log(`Listening on port 3000`));
